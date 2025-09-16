2c448f8a823e503c54e7d62a09f747032d985dca
isakr <isakr@DESKTOP-QENPR6F>
2025-09-16 11:45:32 +0200

    Lagt till olika anrop för CalculateBMI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,18 @@ namespace BMI
         static void Main(string[] args)
         {
             double bmi1 = CalculateBMI(70, 1.75);
-            Console.WriteLine($"BMI (metric, default): {bmi1}");
+            Console.WriteLine($"BMI (metric, default): {bmi1:F2}");
 
             double bmi2 = CalculateBMI(height: 1.80, weight: 85);
-            Console.WriteLine($"BMI (metric, named args): {bmi2}");
+            Console.WriteLine($"BMI (metric, named args): {bmi2:F2}");
 
-            double
+            double bmi3 = CalculateBMI(unit: "imperial", weight: 180, height: 70);
+            Console.WriteLine($"BMI (imperial): {bmi3:F2}");
+
+            double bmi4 = CalculateBMI(70, 1.75, "unknown");
+            Console.WriteLine($"BMI (unknown unit): {bmi4:F2}");
+
+            Console.ReadLine();
         }
     }
 }
