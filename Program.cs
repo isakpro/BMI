7b00613ba7c041ce698ea174459a4fca9a0111c9
isakr <isakr@DESKTOP-QENPR6F>
2025-09-16 11:39:44 +0200

    påbörjat BMI program med olika anrop

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bmi1 = CalculateBMI(70, 1.75);
+            Console.WriteLine($"BMI (metric, default): {bmi1}");
+
+            double bmi2 = CalculateBMI(height: 1.80, weight: 85);
+            Console.WriteLine($"BMI (metric, named args): {bmi2}");
+
+            double
+        }
+    }
+}
